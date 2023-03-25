c19228f6bf38961d2d32a254e6cfe75ac5ab5e3c
MrSakine <msakine20@gmail.com>
2023-03-25 00:45:00 +0000

    button base setup

--- src/app/components/buttons/primary-button/primary-button.module.ts
+++ src/app/components/buttons/primary-button/primary-button.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { PrimaryButtonComponent } from './primary-button.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PrimaryButtonComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [PrimaryButtonComponent]
 })
+
 export class PrimaryButtonModule { }
