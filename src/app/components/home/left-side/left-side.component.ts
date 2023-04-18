87f80378e1cadf590387a62de915d41da197286f
FilyMSakine <fsakine@wassatechnologie.com>
2023-04-18 13:43:12 +0000

    startup component update [left side]

--- src/app/components/home/left-side/left-side.component.ts
+++ src/app/components/home/left-side/left-side.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { ICashier } from 'src/app/core/_interfaces/icashier';
+import { IPrestataire } from 'src/app/core/_interfaces/iprestataire';
+import { IPrestation } from 'src/app/core/_interfaces/iprestation';
 
 @Component({
   selector: 'app-left-side',
@@ -6,15 +9,54 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./left-side.component.scss']
 })
 
-export class LeftSideComponent implements OnInit {
+export class LeftSideComponent implements OnInit, OnChanges {
   showSwitcher: boolean = false;
+  @Input() services!: IPrestation[];
+  @Input() serviceProviders!: IPrestataire[];
+  @Input() cashiers!: ICashier[];
   @Output() showMenu: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    this.onInputChange(changes);
+  }
+
+  ngOnInit(): void {
+    this.handleSwitcher();
+  }
 
   startupMenuEvent(e: any) {
     this.showMenu.emit(e);
+  }
+
+  handleSwitcher() {
+    if (
+      (this.services !== undefined && this.services.length <= 0) &&
+      (this.serviceProviders !== undefined && this.serviceProviders.length <= 0) &&
+      (this.cashiers !== undefined && this.cashiers.length <= 0)
+    ) {
+      this.showSwitcher = false;
+    } else if (
+      (this.services === undefined) && (this.serviceProviders === undefined) && (this.cashiers === undefined)
+    ) {
+      this.showSwitcher = false;
+    } else if (
+      (this.services !== undefined && this.services.length <= 0) ||
+      (this.serviceProviders !== undefined && this.serviceProviders.length <= 0) ||
+      (this.cashiers !== undefined && this.cashiers.length <= 0)
+    ) {
+      this.showSwitcher = false;
+    } else {
+      this.showSwitcher = true;
+    }
+  }
+
+  onInputChange(changes: SimpleChanges) {
+    this.services = changes['services'].currentValue;
+    this.serviceProviders = changes['serviceProviders'].currentValue;
+    this.cashiers = changes['cashiers'].currentValue;
+
+    this.handleSwitcher();
   }
 }
