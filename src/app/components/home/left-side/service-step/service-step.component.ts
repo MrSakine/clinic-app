c9ee359dee0c68fb2c2a42834aa5b5c23a4cdfa6
FilyMSakine <fsakine@wassatechnologie.com>
2023-04-25 18:55:49 +0000

    switcher first step component update

--- src/app/components/home/left-side/service-step/service-step.component.ts
+++ src/app/components/home/left-side/service-step/service-step.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ICashier } from 'src/app/core/_interfaces/icashier';
+import { IPrestataire } from 'src/app/core/_interfaces/iprestataire';
+import { IPrestation } from 'src/app/core/_interfaces/iprestation';
 
 @Component({
   selector: 'app-service-step',
   templateUrl: './service-step.component.html',
   styleUrls: ['./service-step.component.scss']
 })
-export class ServiceStepComponent {
+
+export class ServiceStepComponent implements OnInit {
+  ref!: MatDialogRef<any>;
+  @Input() services!: IPrestation[];
+  @Input() serviceProviders!: IPrestataire[];
+  @Input() cashiers!: ICashier[];
+
+  constructor(
+    private matDialog: MatDialog
+  ) { }
+
+  ngOnInit(): void { }
+
+  openChooseServiceDialog() { }
+
+  prepareDialogData() { }
 
 }
