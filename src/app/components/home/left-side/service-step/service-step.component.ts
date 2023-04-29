b68448fc444a660d4a6ed2319122e5f911e9b90f
FilyMSakine <fsakine@wassatechnologie.com>
2023-04-29 11:25:59 +0000

    switcher child component update

--- src/app/components/home/left-side/service-step/service-step.component.ts
+++ src/app/components/home/left-side/service-step/service-step.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
+import { ChooseServiceDialogData } from 'src/app/core/_dialog-data/choose-service-dialog-data';
 import { ICashier } from 'src/app/core/_interfaces/icashier';
 import { IPrestataire } from 'src/app/core/_interfaces/iprestataire';
 import { IPrestation } from 'src/app/core/_interfaces/iprestation';
+import { ChooseServiceDialogComponent } from './choose-service-dialog/choose-service-dialog.component';
 
 @Component({
   selector: 'app-service-step',
@@ -22,8 +24,25 @@ export class ServiceStepComponent implements OnInit {
 
   ngOnInit(): void { }
 
-  openChooseServiceDialog() { }
+  openChooseServiceDialog() {
+    let data = this.prepareDialogData("35%");
+    this.ref = this.matDialog.open(ChooseServiceDialogComponent, data);
+    this.ref.afterClosed().subscribe(val => this.handleSSPAdded(val));
+  }
 
-  prepareDialogData() { }
+  prepareDialogData(width: string): MatDialogConfig {
+    let config = new MatDialogConfig();
+    let data = new ChooseServiceDialogData();
+
+    config.width = width;
+    data.services = this.services;
+    data.serviceProviders = this.serviceProviders;
+
+    config.data = data;
+
+    return config;
+  }
+
+  handleSSPAdded(val: any) { }
 
 }
