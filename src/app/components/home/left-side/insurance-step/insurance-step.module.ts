91713ec7380bab309f00e35827af57eef836c34c
FilyMSakine <fsakine@wassatechnologie.com>
2023-05-05 01:17:12 +0000

    switcher insurance step update

--- src/app/components/home/left-side/insurance-step/insurance-step.module.ts
+++ src/app/components/home/left-side/insurance-step/insurance-step.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InsuranceStepComponent } from './insurance-step.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [InsuranceStepComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
   ],
   exports: [InsuranceStepComponent]
 })
